ac3285efb68fb5f6b045a2174720c6ad9af35818
davehand <davehand13@gmail.com>
2014-02-25 15:45:11 -0500

    Finished Part A implementation

--- WebServer.java
+++ WebServer.java
@@ -30,5 +30,50 @@ public final class WebServer {
 }
 
 final class HttpRequest implements Runnable {
-  
+
+  final static String CRLF = "\r\n";
+  Socket socket;
+
+  //Constructor
+  public HttpRequest(Socket socket) throws Exception {
+    this.socket = socket;
+  }
+
+  //Implement the run() method of the Runnable interface
+  public void run() {
+    try {
+      processRequest();
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  private void processRequest() throws Exception {
+    //Get a reference to the socket's input and output streams
+    InputStream is = socket.getInputStream();
+    DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+
+    //Set up input stream filters
+    FilterInputStream filterStream;
+    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    //Get the request line of the HTTP request message
+    String requestLine = br.readLine();
+
+    //Display the request line
+    System.out.println();
+    System.out.println(requestLine);
+
+    //Get and display the header lines
+    String headerLine = null;
+    while ((headerLine = br.readLine()).length() != 0) {
+      System.out.println(headerLine);
+    }
+
+    //Close streams and socket
+    os.close();
+    br.close();
+    socket.close();
+  }
 }
